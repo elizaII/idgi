767ee4434d3b067068cda8554d3e52dc90774029
Jonathan Kran <giuinktseo@gmail.com>
2016-03-22 17:11:59 +0100

    Creates the project

---
+++ src/test/model/ProjectTest.java
@@ -0,0 +1,21 @@
+package test.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import model.Project;
+
+public class ProjectTest {
+	private static final int NUM_INCREMENTATIONS = 128;
+
+	@Test
+	public void testIncrementResult() {
+		final Project project = new Project();
+
+		for (int i = 0; i < NUM_INCREMENTATIONS; i++) {
+			project.incrementPresses();
+		}
+
+		Assert.assertEquals(NUM_INCREMENTATIONS, project.getPresses());
+	}
+}
