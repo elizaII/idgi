d7a24ef8ddd8ef3427102ca36f643e3c797919af
Jonathan Kran <giuinktseo@gmail.com>
2016-04-26 15:39:26 +0200

    Buttins!

--- app/src/main/java/com/idgi/QuizActivity.java
+++ app/src/main/java/com/idgi/QuizActivity.java
@@ -1,10 +1,12 @@
 package com.idgi;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class QuizActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+
+		LinearLayout container = (LinearLayout) findViewById(R.id.answer_container);
+
+		LinearLayout row = new LinearLayout(getBaseContext());
+		row.setOrientation(LinearLayout.HORIZONTAL);
+
+		row.addView(new Button(getBaseContext()));
+
+		container.addView(row);
 	}
 }
