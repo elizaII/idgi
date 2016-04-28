8c9d6ece3390f5f0ff6184ef04bf8b1dd13ba798
Tuyen Ngo <tuyen@student.chalmers.se>
2016-04-28 20:01:44 +0200

    Add LessonActivity with the same menu drawer as profile.

---
+++ app/src/main/java/com/idgi/LessonActivity.java
@@ -0,0 +1,15 @@
+package com.idgi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.idgi.R;
+
+public class LessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+    }
+}
