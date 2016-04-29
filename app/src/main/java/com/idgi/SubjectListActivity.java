10d7266c2b0f432f970016d92abf516fa8b436dd
Emil Logren <emil.logren@gmail.com>
2016-04-29 13:47:30 +0200

    Making navigation to lists of subjects related to a school possible.

--- app/src/main/java/com/idgi/SubjectListActivity.java
+++ app/src/main/java/com/idgi/SubjectListActivity.java
@@ -2,12 +2,45 @@ package com.idgi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.idgi.core.Subject;
+import com.idgi.services.Database;
+import com.idgi.util.SubjectListAdapter;
+
+import java.util.ArrayList;
 
 public class SubjectListActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager manager;
+
+    private Database database = Database.getInstance();
+
+    private ArrayList<String> subjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+
+        Bundle bundle = getIntent().getExtras();
+        String s = bundle.getString("schoolName");
+
+        TextView txt = (TextView)findViewById(R.id.subject_list_school);
+        txt.setText(s);
+
+        Subject math = database.getSubject("1");
+        subjects.add(math.getValue());
+
+        manager = new LinearLayoutManager(this);
+        adapter = new SubjectListAdapter(this, subjects);
+
+        recycler = (RecyclerView) findViewById(R.id.subject_list_recycler_view);
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(manager);
     }
 }
---
+++ app/src/main/java/com/idgi/util/SubjectListAdapter.java
@@ -0,0 +1,72 @@
+package com.idgi.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.idgi.R;
+import com.idgi.SchoolListActivity;
+import com.idgi.SubjectListActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Emil on 27/04/2016.
+ */
+public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.ViewHolder> {
+
+    private ArrayList<String> data;
+    private LayoutInflater inflater;
+
+    public SubjectListAdapter(Context context, ArrayList<String> data){
+        this.data = data;
+        inflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflater.inflate(R.layout.list_row, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+
+        return vh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.subjectTextView.setText(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView subjectTextView;
+
+        public ViewHolder(View v){
+            super(v);
+            v.setOnClickListener(this);
+            subjectTextView =(TextView) v.findViewById(R.id.rowTextView);
+        }
+
+        public void onClick(View v){
+            String s = subjectTextView.getText().toString();
+            System.out.println(s);
+            Intent intent = new Intent(v.getContext(), SchoolListActivity.class);
+            intent.putExtra("subjectName", s);
+            v.getContext().startActivity(intent);
+        }
+
+    }
+
+
+}
