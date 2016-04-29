a84733222a34a1274310d78220a48e3de2aa4e05
AllexNordgren <allexnordgren@outlook.com>
2016-04-29 12:39:07 +0200

    add navigation drawer to StartActivity

--- app/src/main/java/com/idgi/StartActivity.java
+++ app/src/main/java/com/idgi/StartActivity.java
@@ -1,13 +1,23 @@
 package com.idgi;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class StartActivity extends AppCompatActivity {
+import com.idgi.util.Navigation;
+
+public class StartActivity extends AppCompatActivity
+		implements NavigationView.OnNavigationItemSelectedListener {
 
 	private Toolbar toolbar;
 
@@ -21,6 +31,24 @@ public class StartActivity extends AppCompatActivity {
 
 		if (savedInstanceState != null)
 			return;
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
+
+		//Copied from ProfileActivity
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.setDrawerListener(toggle);
+		toggle.syncState();
+
+		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
     }
 
     public void showVideoActivity(View view) {
@@ -63,5 +91,30 @@ public class StartActivity extends AppCompatActivity {
 				return super.onOptionsItemSelected(item);
 
 		}
+	}
+	@Override
+	public void onBackPressed() {
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(MenuItem item) {
+		Navigation.onMenuItemSelected(item);
+
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawer.closeDrawer(GravityCompat.START);
+		return true;
 	}
 }
