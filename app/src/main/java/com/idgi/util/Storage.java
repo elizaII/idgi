0903c4c985252124b3be8bc0a74028940e4ee5a4
Tuyen Ngo <tuyen@student.chalmers.se>
2016-04-29 11:39:16 +0200

    Passing video to Storage.

--- app/src/main/java/com/idgi/util/Storage.java
+++ app/src/main/java/com/idgi/util/Storage.java
@@ -1,6 +1,7 @@
 package com.idgi.util;
 
 import com.idgi.core.Quiz;
+import com.idgi.core.Video;
 
 /**
  * Created by Jonathan Krän on 28/04/2016.
@@ -8,6 +9,7 @@ import com.idgi.core.Quiz;
 public class Storage {
 
 	private static Quiz currentQuiz;
+	private static Video currentVideo;
 
 	public static Quiz getCurrentQuiz() {
 		return currentQuiz;
@@ -15,5 +17,13 @@ public class Storage {
 
 	public static void setCurrentQuiz(Quiz quiz) {
 		currentQuiz = quiz;
+	}
+
+	public static Video getCurrentVideo() {
+		return currentVideo;
+	}
+
+	public static void setCurrentVideo(Video video) {
+		currentVideo = video;
 	}
 }
