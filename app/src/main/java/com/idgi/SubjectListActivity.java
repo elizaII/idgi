7fe879b0f72d194486f8f0b4a570f44109d8b4f3
Tuyen Ngo <tuyen@student.chalmers.se>
2016-05-05 20:20:23 +0200

    Refactor SubjectListActivity so it uses App...WithDrawer

--- app/src/main/java/com/idgi/SubjectListActivity.java
+++ app/src/main/java/com/idgi/SubjectListActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import com.idgi.core.Subject;
 import com.idgi.services.Database;
+import com.idgi.util.AppCompatActivityWithDrawer;
 import com.idgi.util.Navigation;
 import com.idgi.util.SubjectListAdapter;
 
@@ -23,7 +24,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SubjectListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class SubjectListActivity extends AppCompatActivityWithDrawer{
     private Toolbar toolbar;
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
@@ -37,6 +38,8 @@ public class SubjectListActivity extends AppCompatActivity implements Navigation
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+
+        initializeDrawer();
 
         Bundle bundle = getIntent().getExtras();
         String s = bundle.getString("schoolName");
@@ -63,38 +66,5 @@ public class SubjectListActivity extends AppCompatActivity implements Navigation
         toolbar = (Toolbar) findViewById(R.id.test_toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        Navigation.onMenuItemSelected(this, item);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
