ec44ee02854ba8b83707296e743d3db9c75c5fa8
Jonathan Kran <giuinktseo@gmail.com>
2016-05-06 15:23:05 +0200

    Move some constants to Config

--- app/src/main/java/com/idgi/util/Config.java
+++ app/src/main/java/com/idgi/util/Config.java
@@ -7,4 +7,8 @@ public final class Config {
 
 	public static final String YOUTUBE_API_KEY = "AIzaSyC0sOK4dltFQ9aThbYRshysc1i35wcdaDU";
 
+	/* Points */
+	public static final int MAX_POINTS_FOR_VIDEO = 600;
+	public static final int POINTS_PER_TICK = 85;
+
 }
