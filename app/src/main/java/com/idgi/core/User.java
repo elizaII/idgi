25b9fac90b5e077edff50af26d0a266492394907
Jonathan Kran <giuinktseo@gmail.com>
2016-05-06 16:09:44 +0200

    Add methods for point retrieval

--- app/src/main/java/com/idgi/core/User.java
+++ app/src/main/java/com/idgi/core/User.java
@@ -55,7 +55,7 @@ public class User {
     public Drawable getImage() {
         return image;
     }
-    
+
     public int getPointsForVideo(Video video) {
         return statistics.getVideoPoints(video);
     }
