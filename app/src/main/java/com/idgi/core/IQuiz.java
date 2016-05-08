25a047fcf19b1e5d29aa146857f5b5d95102e88d
Tuyen Ngo <tuyen@student.chalmers.se>
2016-05-08 22:57:09 +0200

    Refactor Quiz with an interface

---
+++ app/src/main/java/com/idgi/core/IQuiz.java
@@ -0,0 +1,31 @@
+package com.idgi.core;
+
+import java.util.List;
+
+public interface IQuiz {
+
+    int getID();
+
+    int length();
+
+    void addQuestion(Question question);
+
+    void addQuestions(List<Question> questions);
+
+    void nextQuestion();
+
+    Question getCurrentQuestion();
+
+    List<Question> getQuestions();
+
+    boolean hasQuestion(Question question);
+
+    boolean isLastQuestion();
+
+    boolean isFinished();
+
+    int getCorrectAnswerAmount();
+
+    int getPointsEarned();
+
+}
