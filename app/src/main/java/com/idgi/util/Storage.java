cb6b0093fb8d6a0724f90cd9a5108843723199be
Jonathan Kran <giuinktseo@gmail.com>
2016-05-08 20:29:18 +0200

    Refactor Storage

--- app/src/main/java/com/idgi/util/Storage.java
+++ app/src/main/java/com/idgi/util/Storage.java
@@ -4,20 +4,40 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 
 import com.idgi.R;
+import com.idgi.core.Course;
 import com.idgi.core.Lesson;
 import com.idgi.core.Quiz;
+import com.idgi.core.School;
+import com.idgi.core.Subject;
 import com.idgi.core.User;
 import com.idgi.core.Video;
 
 public class Storage {
 
-	private static Quiz currentQuiz;
-	// TODO create actual implemenation
-	private static Video currentVideo = new Video("ffLLmV4mZwU");
-    private static Lesson currentLesson;
+	private static School currentSchool;
+
+	public static Subject getCurrentSubject() {
+		return currentSubject;
+	}
+
+	public static void setCurrentSubject(Subject currentSubject) {
+		Storage.currentSubject = currentSubject;
+	}
+
+	private static Subject currentSubject;
+
+	public static Course getCurrentCourse() {
+		return currentCourse;
+	}
+
+	public static void setCurrentCourse(Course currentCourse) {
+		Storage.currentCourse = currentCourse;
+	}
+
+	private static Course currentCourse;
+	private static Lesson currentLesson;
 
 	// TODO : Create actual implemenation of activeUser, probably through phone local storage?
-
 	public static void setActiveUser(User activeUser) {
 		Storage.activeUser = activeUser;
 	}
@@ -25,19 +45,11 @@ public class Storage {
 	private static User activeUser = new User("Test");
 
 	public static Quiz getCurrentQuiz() {
-		return currentQuiz;
-	}
-
-	public static void setCurrentQuiz(Quiz quiz) {
-		currentQuiz = quiz;
+		return currentLesson.getQuiz();
 	}
 
 	public static Video getCurrentVideo() {
-		return currentVideo;
-	}
-
-	public static void setCurrentVideo(Video video) {
-		currentVideo = video;
+		return currentLesson.getVideo();
 	}
 
 	public static void setCurrentLesson(Lesson lesson) {
@@ -49,5 +61,13 @@ public class Storage {
     }
 	public static User getActiveUser() {
 		return activeUser;
+	}
+
+	public static School getCurrentSchool() {
+		return currentSchool;
+	}
+
+	public static void setCurrentSchool(School school) {
+		currentSchool = school;
 	}
 }
