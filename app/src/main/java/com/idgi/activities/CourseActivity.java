abf006bd9c55305d57efe03d12cd253071b9c8eb
Jonathan Kran <giuinktseo@gmail.com>
2016-05-10 18:36:34 +0200

    Modify CourseActivity to display alternative view if list is empty

--- app/src/main/java/com/idgi/activities/CourseActivity.java
+++ app/src/main/java/com/idgi/activities/CourseActivity.java
@@ -1,5 +1,6 @@
 package com.idgi.activities;
 
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.idgi.fragments.CourseInfoFragment;
 import com.idgi.fragments.CourseLessonListFragment;
@@ -21,21 +24,37 @@ import com.idgi.util.Storage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CourseActivity extends AppCompatActivityWithDrawer {
+
+    private enum Section {
+        LESSON, QUIZ, INFO;
+
+        public static Section fromInteger(int x) {
+            switch (x) {
+                case 0:
+                    return LESSON;
+                case 1:
+                    return QUIZ;
+                case 2:
+                    return INFO;
+                default:
+                    return INFO;
+            }
+        }
+    }
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private TextView emptyViewText;
 
     private RecyclerView recycler;
     private RecyclerView.Adapter recyclerAdapter;
     private RecyclerView.LayoutManager recyclerManager;
 
-    private Database database = Database.getInstance();
-
-    //should be list of Lessons, when working
-    private ArrayList<Lesson> lessons = new ArrayList<>();
+    private ViewPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,19 +68,42 @@ public class CourseActivity extends AppCompatActivityWithDrawer {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(courseName);
 
+        initializeDrawer();
+
+        setupViewPager();
+    }
+
+    private void setupViewPager() {
         viewPager = (ViewPager) findViewById(R.id.course_pager);
 
-        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         pagerAdapter.addFragment(new CourseLessonListFragment(), "Lessons");
         pagerAdapter.addFragment(new CourseQuizListFragment(), "Quiz");
         pagerAdapter.addFragment(new CourseInfoFragment(), "Info");
 
         viewPager.setAdapter(pagerAdapter);
 
+        viewPager.addOnPageChangeListener(pageChangeListener);
+        //refreshViewPager();
+
         tabLayout = (TabLayout) findViewById(R.id.course_tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+    }
 
-        initializeDrawer();
+    /* Makes sure the appropriate values (like empty-list text message) are used */
+    private void refreshViewPager() {
+        //We do it this way since there's no refresh() equivalent method.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                View view = pagerAdapter.getItem(1).getView();
+                if (view != null) {
+                    viewPager.setCurrentItem(1);
+                    viewPager.setCurrentItem(0);
+                }
+            }
+        }, 50);
+
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -91,5 +133,30 @@ public class CourseActivity extends AppCompatActivityWithDrawer {
         public CharSequence getPageTitle(int position) {
             return fragmentNameList.get(position);
         }
+    }
+
+    private final ViewPager.OnPageChangeListener pageChangeListener =  new ViewPager.SimpleOnPageChangeListener() {
+        @Override
+        public void onPageSelected(int position) {
+
+            String messages[] = {
+                    getResources().getString(R.string.course_no_lessons),
+                    getResources().getString(R.string.course_no_quizzes),
+                    getResources().getString(R.string.course_no_info),
+            };
+
+            String text = messages[position];
+            View view = pagerAdapter.getItem(position).getView();
+
+            updateEmptyTextInView(view, text);
+
+        }
+    };
+
+    /* Updates the text that is shown if a list is empty */
+    private void updateEmptyTextInView(View view, String text) {
+
+        TextView textView = (TextView) view.findViewById(R.id.lesson_list_empty_view_text);
+        textView.setText(text);
     }
 }
