5d57255f5038bfd30b4d00ff7aa10c5f7a19d0e2
AllexNordgren <AllexNordgren@outlook.com>
2016-05-12 16:09:54 +0200

    adding sad face to empty list in my courses

--- app/src/main/java/com/idgi/activities/MyCoursesActivity.java
+++ app/src/main/java/com/idgi/activities/MyCoursesActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.idgi.R;
+import com.idgi.Widgets.EmptyRecyclerView;
 import com.idgi.core.Course;
 import com.idgi.recycleViews.adapters.CourseListAdapter;
 import com.idgi.recycleViews.adapters.MyCoursesAdapter;
@@ -18,7 +21,7 @@ import java.util.ArrayList;
 public class MyCoursesActivity extends AppCompatActivityWithDrawer {
 
     private CourseListAdapter adapter;
-    private RecyclerView recyclerView;
+    private EmptyRecyclerView recycler;
     private RecyclerView.LayoutManager manager;
     private ArrayList<Course> data;
 
@@ -38,11 +41,18 @@ public class MyCoursesActivity extends AppCompatActivityWithDrawer {
 
 
         manager = new LinearLayoutManager(this);
-        recyclerView = (RecyclerView) findViewById(R.id.my_courses_list_recycler_view);
-        recyclerView.setLayoutManager(manager);
+        recycler = (EmptyRecyclerView) findViewById(R.id.my_courses_list_recycler_view);
+        recycler.setLayoutManager(manager);
+
+        View emptyView = findViewById(R.id.lesson_list_empty_view);
+
+        recycler.setEmptyView(emptyView);
+
+        TextView textView = (TextView) findViewById(R.id.lesson_list_empty_view_text);
+        textView.setText(getResources().getString(R.string.course_no_courses));
 
         adapter = new CourseListAdapter(this, this.data);
-        recyclerView.setAdapter(adapter);
+        recycler.setAdapter(adapter);
     }
 
 }
