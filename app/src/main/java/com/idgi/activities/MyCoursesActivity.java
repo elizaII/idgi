848851b43fd4a730721cf1dbe583f7238c5938db
AllexNordgren <AllexNordgren@outlook.com>
2016-05-12 15:59:25 +0200

    adding navigation drawer and toolbar to the my courses view

--- app/src/main/java/com/idgi/activities/MyCoursesActivity.java
+++ app/src/main/java/com/idgi/activities/MyCoursesActivity.java
@@ -4,16 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.idgi.R;
 import com.idgi.core.Course;
 import com.idgi.recycleViews.adapters.CourseListAdapter;
 import com.idgi.recycleViews.adapters.MyCoursesAdapter;
+import com.idgi.util.AppCompatActivityWithDrawer;
 import com.idgi.util.Storage;
 
 import java.util.ArrayList;
 
-public class MyCoursesActivity extends AppCompatActivity {
+public class MyCoursesActivity extends AppCompatActivityWithDrawer {
 
     private CourseListAdapter adapter;
     private RecyclerView recyclerView;
@@ -25,8 +27,15 @@ public class MyCoursesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.title_activity_my_course);
+
+        initializeDrawer();
         this.data = new ArrayList<>();
         data = Storage.getActiveUser().getMyCourses();
+
+
 
         manager = new LinearLayoutManager(this);
         recyclerView = (RecyclerView) findViewById(R.id.my_courses_list_recycler_view);
