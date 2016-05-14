5425559da8c9226d0164f8b1e0fb20d9fe12dff4
Emil Logren <emil.logren@gmail.com>
2016-05-15 01:16:28 +0200

    Refactor SearchableAdapter and fix Activity

--- app/src/main/java/com/idgi/activities/SearchableActivity.java
+++ app/src/main/java/com/idgi/activities/SearchableActivity.java
@@ -2,26 +2,20 @@ package com.idgi.activities;
 
 import android.app.SearchManager;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
-import com.firebase.client.Firebase;
-import com.firebase.client.Query;
 import com.idgi.R;
 import com.idgi.activities.extras.DrawerActivity;
-import com.idgi.activities.recycleViews.adapters.SearchableAdapter;
+import com.idgi.recycleViews.adapters.SearchableAdapter;
 import com.idgi.core.Course;
 import com.idgi.core.Lesson;
 import com.idgi.core.School;
 import com.idgi.core.Subject;
-import com.idgi.services.FireDatabase;
 import com.idgi.services.MockData;
 import com.idgi.util.Nameable;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,7 +35,6 @@ public class SearchableActivity extends DrawerActivity {
         Intent intent = getIntent();
         if(intent.ACTION_SEARCH.equals(intent.getAction())){
             String query = intent.getStringExtra(SearchManager.QUERY);
-            query.toLowerCase();
             searchDatabase(query.toLowerCase());
         }
 
--- app/src/main/java/com/idgi/recycleViews/adapters/SearchableAdapter.java
+++ app/src/main/java/com/idgi/recycleViews/adapters/SearchableAdapter.java
@@ -1,4 +1,4 @@
-package com.idgi.activities.recycleViews.adapters;
+package com.idgi.recycleViews.adapters;
 
 import android.content.Context;
 import android.content.Intent;
