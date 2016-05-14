a181cfbbb85332d93857d7871cb7eca07acf098b
Tove <ektove@student.chalmers.se>
2016-05-14 23:15:02 +0200

    Create Hat class and related RecyclerView

---
+++ app/src/main/java/com/idgi/activities/recycleViews/adapters/HatListAdapter.java
@@ -0,0 +1,45 @@
+package com.idgi.activities.recycleViews.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.idgi.R;
+import com.idgi.activities.recycleViews.viewHolders.HatViewHolder;
+import com.idgi.core.Hat;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tove on 2016-05-13.
+ */
+public class HatListAdapter extends RecyclerView.Adapter<HatViewHolder> {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private ArrayList<Hat> hats;
+
+    public HatListAdapter(Context context, ArrayList<Hat> hats) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.hats = hats;
+    }
+
+    @Override
+    public HatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_row_hat, parent, false);
+        return new HatViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(HatViewHolder holder, int position) {
+        holder.setUpView(hats.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return hats.size();
+    }
+}
---
+++ app/src/main/java/com/idgi/core/Hat.java
@@ -0,0 +1,46 @@
+package com.idgi.core;
+
+import android.media.Image;
+
+/**
+ * Created by tove on 2016-05-14.
+ */
+public class Hat {
+
+    private int imageId;
+    private String name;
+    private int points;
+    private String description;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
