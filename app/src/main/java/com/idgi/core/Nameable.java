31bcdcf0ed19ff65f00be74f2c54670f835966a9
Jonathan Kran <giuinktseo@gmail.com>
2016-05-17 16:54:49 +0200

    Solve bugs found through FindBugs application

--- app/src/main/java/com/idgi/core/Nameable.java
+++ app/src/main/java/com/idgi/core/Nameable.java
@@ -1,5 +1,25 @@
 package com.idgi.core;
 
 public interface Nameable {
+	enum Type {
+		SCHOOL(0), SUBJECT(1), COURSE(2), LESSON(3);
+
+		private int value;
+		private static Type[] values = values();
+
+		Type(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public static Type fromInteger(int x) {
+			return values[x];
+		}
+	}
+
 	String getName();
+	Type getType();
 }
