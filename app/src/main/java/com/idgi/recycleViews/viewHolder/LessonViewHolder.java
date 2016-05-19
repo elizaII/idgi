f87c55074f19a0f5216dc7416ef01963d03ed10d
Emil Logren <emil.logren@gmail.com>
2016-05-19 17:42:50 +0200

    Fix thumbnails in LessonList

--- app/src/main/java/com/idgi/recycleViews/viewHolder/LessonViewHolder.java
+++ app/src/main/java/com/idgi/recycleViews/viewHolder/LessonViewHolder.java
@@ -1,24 +1,31 @@
 package com.idgi.recycleViews.viewHolder;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.idgi.Config;
 import com.idgi.R;
+import com.idgi.core.Lesson;
 import com.idgi.core.Nameable;
 import com.idgi.event.NameableSelectionBus;
 
 public class LessonViewHolder extends NameableViewHolder {
 
-	public static final int LAYOUT = R.layout.list_row;
+	public static final int LAYOUT = R.layout.list_row_lesson;
 
 	public TextView nameTextView;
+	public YouTubeThumbnailView lessonThumbnail;
 	public RelativeLayout listBackground;
 
-	public LessonViewHolder(View view, NameableSelectionBus bus){
+	public LessonViewHolder(View view, NameableSelectionBus bus) {
 		super(view, bus);
 		view.setOnClickListener(onViewClick);
 	}
@@ -31,13 +38,24 @@ public class LessonViewHolder extends NameableViewHolder {
 	};
 
 	@Override
-	public void bind(Nameable nameable) {
+	public void bind(final Nameable nameable) {
 		nameTextView.setText(nameable.getName());
+		lessonThumbnail.initialize(Config.YOUTUBE_API_KEY, new YouTubeThumbnailView.OnInitializedListener() {
+			public void onInitializationSuccess(YouTubeThumbnailView view, YouTubeThumbnailLoader loader) {
+				Lesson lesson = (Lesson) nameable;
+				loader.setVideo(lesson.getVideo().getUrl());
+			}
+
+			public void onInitializationFailure(YouTubeThumbnailView view, YouTubeInitializationResult result) {
+				Log.d("thumbnail", "Initialization didn't work");
+			}
+		});
 	}
 
 	@Override
 	public void initialize() {
 		nameTextView = (TextView) findViewById(R.id.rowTextView);
+		lessonThumbnail = (YouTubeThumbnailView) findViewById(R.id.lesson_thumbnail);
 		listBackground = (RelativeLayout) findViewById(R.id.list_background);
 	}
 
