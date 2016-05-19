6df3f5c00922d0eb264be920d3442db6275521c8
Emil Logren <emil.logren@gmail.com>
2016-05-19 18:40:10 +0200

    Fix navigation from Search

--- app/src/main/java/com/idgi/activities/SearchableActivity.java
+++ app/src/main/java/com/idgi/activities/SearchableActivity.java
@@ -4,11 +4,14 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.idgi.Config;
 import com.idgi.R;
 import com.idgi.activities.extras.DrawerActivity;
 import com.idgi.core.Nameable;
+import com.idgi.core.Quiz;
+import com.idgi.event.NameableSelectionBus;
 import com.idgi.recycleViews.RecyclerViewUtility;
 import com.idgi.recycleViews.adapters.NameableAdapter;
 import com.idgi.core.Course;
@@ -18,12 +21,13 @@ import com.idgi.core.Subject;
 import com.idgi.services.FireDatabase;
 import com.idgi.services.IDatabase;
 import com.idgi.services.MockData;
+import com.idgi.session.SessionData;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class SearchableActivity extends DrawerActivity {
+public class SearchableActivity extends DrawerActivity implements NameableSelectionBus.Listener{
 
     private ArrayList<Nameable> searchResults;
 
@@ -40,6 +44,7 @@ public class SearchableActivity extends DrawerActivity {
 
         RecyclerView recycler = (RecyclerView) findViewById(R.id.searchable_recycler_view);
 		NameableAdapter adapter = new NameableAdapter(this, searchResults);
+        adapter.addListener(this);
 		RecyclerViewUtility.connect(this, recycler, adapter);
 
         initializeDrawer();
@@ -53,7 +58,7 @@ public class SearchableActivity extends DrawerActivity {
     private void searchDatabase(String query){
         ArrayList<Nameable> results = new ArrayList<>();
 
-        IDatabase database = getDatabase();
+        FireDatabase database = FireDatabase.getInstance();
         List<School> schools = database.getSchools();
 
         for(School school: schools){
@@ -76,6 +81,35 @@ public class SearchableActivity extends DrawerActivity {
             }
         }
         this.searchResults = results;
+    }
+
+    @Override
+    public void onNameableSelected(Nameable nameable) {
+        if(nameable != null) {
+            if (nameable instanceof School) {
+                School school = (School) nameable;
+                SessionData.setCurrentSchool(school);
+                startActivity((new Intent(this, SubjectListActivity.class)));
+                Log.d("search", "school");
+            } else if (nameable instanceof Subject) {
+                Subject subject = (Subject) nameable;
+                SessionData.setCurrentSubject(subject);
+                startActivity((new Intent(this, CourseListActivity.class)));
+                Log.d("search", "subject");
+            } else if (nameable instanceof Course) {
+                Course course = (Course) nameable;
+                SessionData.setCurrentCourse(course);
+                startActivity((new Intent(this, CourseActivity.class)));
+                Log.d("search", "course");
+            } else if (nameable instanceof Lesson) {
+                Lesson lesson = (Lesson) nameable;
+                SessionData.setCurrentLesson(lesson);
+                startActivity((new Intent(this, LessonActivity.class)));
+                Log.d("search", "lesson");
+            } else {
+                return;
+            }
+        }
     }
 }
 
