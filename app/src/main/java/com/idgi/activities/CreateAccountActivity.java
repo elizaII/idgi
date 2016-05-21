0ae8128881b6f48454413d6d18a1ca51370b38bd
Tove <ektove@student.chalmers.se>
2016-05-21 17:41:31 +0200

    Added spinner and password to CreateAccountActivity

--- app/src/main/java/com/idgi/activities/CreateAccountActivity.java
+++ app/src/main/java/com/idgi/activities/CreateAccountActivity.java
@@ -6,18 +6,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.idgi.R;
 import com.idgi.core.Account;
 import com.idgi.core.StudentUser;
+import com.idgi.core.TeacherUser;
 import com.idgi.core.User;
 import com.idgi.services.FireDatabase;
 import com.idgi.session.SessionData;
 
-public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText nameText, phoneText, eMailText;
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    private static final String ACCOUNT_TYPE_STUDENT = "Student";
+    private static final String ACCOUNT_TYPE_TEACHER = "Lärare";
+
+    private EditText nameText;
+    private EditText phoneText;
+    private EditText eMailText;
+    private EditText passwordText;
+    private String selectedAccountType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +41,20 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         nameText = (EditText) findViewById(R.id.nameText);
         phoneText = (EditText) findViewById(R.id.phoneText);
         eMailText = (EditText) findViewById(R.id.eMailText);
+        passwordText = (EditText) findViewById(R.id.create_account_edittext_password);
+
+        Spinner accountTypeSpinner = (Spinner) findViewById(R.id.create_account_spinner_account_type);
+        String[] items = new String[]{ACCOUNT_TYPE_STUDENT, ACCOUNT_TYPE_TEACHER};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (accountTypeSpinner != null) {
+            accountTypeSpinner.setAdapter(adapter);
+
+            // Set default account type to Student.
+            accountTypeSpinner.setSelection(0);
+            selectedAccountType = ACCOUNT_TYPE_STUDENT;
+        }
 
         Button createAccountButton = (Button) findViewById(R.id.createAccountButton);
         if (createAccountButton != null)
@@ -37,16 +64,17 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     public void createAccount() {
         String name = this.nameText.getText().toString();
         String email = this.eMailText.getText().toString();
-
-        // TODO implement actual account name
-        String accountName = "test";
-
-        // TODO implement actual password
-        String password = "hej123";
+        String accountName = email;
+        String password = this.passwordText.getText().toString();
 
         Account account = new Account(accountName, password);
-        //Todo... Checks type of account before creating
-        User user = new StudentUser(name);
+
+        User user;
+        if (selectedAccountType.equals(ACCOUNT_TYPE_STUDENT)) {
+            user = new StudentUser(name);
+        } else {
+            user = new TeacherUser(name);
+        }
         user.setEmail(email);
         account.setUser(user);
 
@@ -82,5 +110,15 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 createAccount();
             }
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedAccountType = (String) parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Do nothing.
     }
 }
