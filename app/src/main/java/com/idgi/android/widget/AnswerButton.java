7b0ae2138bc4fbdb2c1edb6f73877865cef222fd
Jonathan Kran <giuinktseo@gmail.com>
2016-05-25 18:45:24 +0200

    Remove outdated logic

--- app/src/main/java/com/idgi/android/widget/AnswerButton.java
+++ app/src/main/java/com/idgi/android/widget/AnswerButton.java
@@ -10,18 +10,15 @@ import android.widget.Button;
 import com.idgi.R;
 import com.idgi.core.Answer;
 
+/*
+Custom button used to display the Answers to a quiz as the quiz is being taken.
+ */
 public class AnswerButton extends Button {
 	private static final int DEFAULT_LOOK = R.drawable.answer_button_normal;
 	private static final int CORRECT_LOOK = R.drawable.answer_button_correct;
 
-	//TODO change back to answer_button_incorrect
-	private static final int INCORRECT_LOOK = R.drawable.answer_button_normal;
-
 	private static final int DEFAULT_LOOK_SELECTED = R.drawable.answer_button_normal_selected;
 	private static final int CORRECT_LOOK_SELECTED = R.drawable.answer_button_correct_selected;
-
-	//TODO change back to answer_button_incorrect_selected
-	private static final int INCORRECT_LOOK_SELECTED = R.drawable.answer_button_normal_selected;
 
 	public static final int SELECTION_FADE_TIME = 400;
 	private static final int FADE_TIME_SHOW_ANSWER = 1000;
@@ -33,6 +30,11 @@ public class AnswerButton extends Button {
 	private DisplayMode displayMode = DisplayMode.NORMAL;
 	private Answer answer;
 
+	// Required for android:tools in layout files
+	private AnswerButton(Context context) {
+		super(context);
+	}
+
 	public AnswerButton(Context context, AttributeSet attributeSet, int defStyleAttr, Answer answer) {
 		super(context, attributeSet, defStyleAttr);
 		this.answer = answer;
@@ -42,13 +44,21 @@ public class AnswerButton extends Button {
 		return this.answer;
 	}
 
+	public void setDisplayMode(DisplayMode mode) {
+		this.displayMode = mode;
+		updateDrawable(FADE_TIME_SHOW_ANSWER);
+	}
+
 	public void updateDrawable(int fadeTime) {
 		Drawable oldBackground = this.getBackground();
 		Drawable newBackground = ContextCompat.getDrawable(getContext(), getDrawable());
 
 		updateTextColor();
+		playTransition(oldBackground, newBackground, fadeTime);
+	}
 
-		Drawable drawables[] = {oldBackground, newBackground};
+	private void playTransition(Drawable from, Drawable to, int fadeTime) {
+		Drawable drawables[] = {from, to};
 		TransitionDrawable transitionBackground = new TransitionDrawable(drawables);
 
 		this.setBackground(transitionBackground);
@@ -58,32 +68,22 @@ public class AnswerButton extends Button {
 
 	private void updateTextColor() {
 		if (displayMode == DisplayMode.SHOW_ANSWER && answer.isCorrect())
-			this.setTextColor(getResources().getColor(R.color.colorQuizQuestionText));
+			this.setTextColor(ContextCompat.getColor(getContext(), R.color.colorQuizQuestionText));
 		else
-			this.setTextColor(getResources().getColor(R.color.colorAnswerButtonBorder));
+			this.setTextColor(ContextCompat.getColor(getContext(), R.color.colorAnswerButtonBorder));
 	}
 
 	private int getDrawable() {
-		int normal, correct, incorrect;
+		int normal, correct;
 
 		if (answer.isSelected()) {
 			normal = DEFAULT_LOOK_SELECTED;
 			correct = CORRECT_LOOK_SELECTED;
-			incorrect = INCORRECT_LOOK_SELECTED;
 		} else {
 			normal = DEFAULT_LOOK;
 			correct = CORRECT_LOOK;
-			incorrect = INCORRECT_LOOK;
 		}
 
-		if (displayMode == DisplayMode.SHOW_ANSWER)
-			return answer.isCorrect() ? correct : incorrect;
-		else
-			return normal;
-	}
-
-	public void setDisplayMode(DisplayMode mode) {
-		this.displayMode = mode;
-		updateDrawable(FADE_TIME_SHOW_ANSWER);
+		return displayMode == DisplayMode.SHOW_ANSWER && answer.isCorrect() ? correct : normal;
 	}
 }
